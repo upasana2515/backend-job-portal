8c88f33cc802fcd8474861015ef7f080b4bf90bc
upasana <upasanagaur55@gmail.com>
2026-05-17 22:51:32 +0530

    done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -179,29 +179,29 @@
 
 
 
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
 
-exports.register = async (req, res) => {
-  try {
-    const { username, email, password, role } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ message: "User already exists" });
+// exports.register = async (req, res) => {
+//   try {
+//     const { username, email, password, role } = req.body;
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) return res.status(400).json({ message: "User already exists" });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ username, email, password: hashedPassword, role });
-    const token = jwt.sign(
-  { id: user._id, username: user.username, role: user.role },
-  process.env.JWT_SECRET,
-  { expiresIn: "7d" }
-);
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const user = await User.create({ username, email, password: hashedPassword, role });
+//     const token = jwt.sign(
+//   { id: user._id, username: user.username, role: user.role },
+//   process.env.JWT_SECRET,
+//   { expiresIn: "7d" }
+// );
 
-    res.status(201).json({ message: "Registration successful", token, role: user.role, username: user.username });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+//     res.status(201).json({ message: "Registration successful", token, role: user.role, username: user.username });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
 // exports.login = async (req, res) => {
 //   try {
