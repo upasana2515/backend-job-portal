8cfabcdcd13abe00fafde85a5dae60875766fc91
upasana <upasanagaur55@gmail.com>
2026-05-17 18:05:07 +0530

    fixed

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -839,7 +839,7 @@
 
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { GoogleGenerativeAI } from "@google/generative-ai";
+
 
 import API from "../api/api";
 import Navbar from "../components/Navbar";
@@ -848,9 +848,7 @@ function Dashboard() {
 
   const navigate = useNavigate();
 
-  // const genAI = new GoogleGenerativeAI(
-  //   process.env.REACT_APP_GEMINI_API_KEY
-  // );
+  
 
   // STATES
   const [jobs, setJobs] = useState([]);
