43c88f9b464fcdb4d8f040cdfddf43393d35f486
upasana <upasanagaur55@gmail.com>
2026-05-17 21:35:07 +0530

    fixed cors

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -20,20 +20,39 @@
 
 
 
+// import axios from "axios";
+
+// // const API = axios.create({
+// //   baseURL: "http://localhost:5000/api",
+// // });
+// const API = axios.create({
+//   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+// });
+
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) req.headers.Authorization = `Bearer ${token}`;
+//   return req;
+// });
+
+
+
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api",
-// });
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: "https://jobhive-backend-3loy.onrender.com/api",
 });
-
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
   return req;
 });
 
 export default API;
+// export default API;
