725c52a9ad1d697284193740823ff2b2f46fa61c
upasana <upasanagaur55@gmail.com>
2026-05-18 19:53:43 +0530

    fixed cors crash

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -18,17 +18,22 @@
 // export default API;
 
 
-
-
 import axios from "axios";
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
-});
 
+const API = axios.create({
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "http://localhost:5000/api",
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
   return req;
 });
+
 export default API;
