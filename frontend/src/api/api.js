62a4b96efdc0202f4555a9d4018705f7303a4152
upasana <upasanagaur55@gmail.com>
2026-05-18 20:01:29 +0530

    fixed cors crash

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -18,6 +18,26 @@
 // export default API;
 
 
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL:
+//     process.env.REACT_APP_API_URL ||
+//     "http://localhost:5000/api",
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return req;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
