dd89edf2d975e1f700c99f00987d1de10e9b4456
upasana <upasanagaur55@gmail.com>
2026-05-21 23:00:54 +0530

    change baseurl to /api

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cors({
     "http://localhost:3000",
     "https://backend-job-portal-a7gr.vercel.app"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 }));
 
